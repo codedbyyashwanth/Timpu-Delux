6c8a7f741b6ab178e3a8b70a2a98c9bb22cf80dd
Yashwanth <yashmy01@gmail.com>
2022-05-18 18:59:43 -0700

    My First Commit

---
+++ script/script.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+        loop: true,
+
+        autoplay: {
+                delay: 2500,
+        },
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+});
